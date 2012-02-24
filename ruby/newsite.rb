1973ff7d834af34f093259d172d545845d09a9f3
Matt McCormick <matt@mattmccormick.ca>
2012-02-24 15:13:29 +0900

    Add newsite.rb script to setup the apache configuration for a new site

---
+++ ruby/newsite.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require_relative 'sudome/sudome.rb'
+
+abort "Development URL and path must be provided" if ARGV.count != 2
+abort "Must be run as root" if !is_root
+
+LOCAL_CONFIG_DIR = "/home/matt/www/config/"
+APACHE_CONF_DIR = "/etc/apache2/sites-available"
+HOSTS = '/etc/hosts'
+
+url = ARGV[0]
+path = File.expand_path(ARGV[1])
+
+abort "Path not correct" if !Dir.exists?(path)
+
+basename = url + '.conf'
+outfile = LOCAL_CONFIG_DIR + basename
+
+if !File.exists?(outfile)
+	conf = File.open("#{LOCAL_CONFIG_DIR}template.conf", 'r') { |f| f.read }
+	new = conf.gsub('URL', url).gsub('PATH', path)
+	File.open(outfile, 'w') { |f| f.write(new) }
+	FileUtils.chown('matt', 'matt', outfile)
+else
+	puts "#{outfile} already exists. Will use this one"
+end
+
+apache_conf = APACHE_CONF_DIR + '/' + basename
+File.symlink(outfile, apache_conf)
+
+system("a2ensite #{basename}")
+
+File.open(HOSTS, 'a') { |f| f.puts "127.0.0.1\t#{url}" }
+
+system("service apache2 restart")
